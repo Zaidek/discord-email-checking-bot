337d48031d4ac3a9c68f1aaaebbd3426a4f916a4
Ethan <ez272@cam.ac.uk>
2021-08-02 16:57:05 +0100

    Added on_message() event and added checks for a '!EMConfigure' command and check for admin permissions

--- Discord Bot/main.py
+++ Discord Bot/main.py
@@ -11,11 +11,7 @@ import os
 client = discord.Client()
   
 # INITIALISE VARIABLES
-accessable_channels = []
-current_guild = None
-for guild in client.guilds:
-    print(guild.name + "X")
-
+accessible_channels = []
 
 # START UP EVENT
 @client.event
@@ -24,21 +20,37 @@ async def on_ready():
     channels = client.get_all_channels()
     update_usable_channels(channels)
 
+@client.event
+async def on_message(message):
+    author = message.author
+    channel = message.channel
+    guildID = channel.guild.id 
+
+
+    if not message.content.startswith('!EMConfigure'): return
+    if not author.permissions_in(channel).administrator: return
+    print("hi")
+    
+
+
 # FIND ALL CHANNELS ACCESABLE BY EMAILIA          
 def update_usable_channels(channels): 
     for channel in channels:
         if channel.category == None: continue
         if channel.category.name != "Text channels": continue
         if client.user not in channel.members: continue
-        accessable_channels.append(channel)
+        accessible_channels.append(channel)
 
+# PRINT OUT ALL CHANNELS ACCESSIBLE BY EMAILIA
 def print_usable_channels():
-    for channel in accessable_channels:
+    for channel in accessible_channels:
         print(channel)
 
+# GET THE TOTKEN FOR EMAILIA FROM ENVIRONMENT FILE
 def get_token():
     token = os.getenv('TOKEN')
     return token
 
+# RUN EMAILIA
 client.run(get_token())
 
