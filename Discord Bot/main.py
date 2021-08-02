9fd15791ba7fd8d2bd8438e9c07f589374c99706
Ethan <ez272@cam.ac.uk>
2021-08-02 15:22:00 +0100

    Added update_usable_channels() to get all channels that are accessable by emailia and added it to on_ready()

--- Discord Bot/main.py
+++ Discord Bot/main.py
@@ -6,14 +6,39 @@ import numpy as np
 import os
 
 
+
 # INITIALISE CLIENT
 client = discord.Client()
-    
+  
+# INITIALISE VARIABLES
+accessable_channels = []
+current_guild = None
+for guild in client.guilds:
+    print(guild.name + "X")
+
+
 # START UP EVENT
 @client.event
 async def on_ready():
     print("{0.user} has arrived!".format(client))
+    channels = client.get_all_channels()
+    update_usable_channels(channels)
 
+# FIND ALL CHANNELS ACCESABLE BY EMAILIA          
+def update_usable_channels(channels): 
+    for channel in channels:
+        if channel.category == None: continue
+        if channel.category.name != "Text channels": continue
+        if client.user not in channel.members: continue
+        accessable_channels.append(channel)
 
-client.run(os.getenv('TOKEN'))
+def print_usable_channels():
+    for channel in accessable_channels:
+        print(channel)
+
+def get_token():
+    token = os.getenv('TOKEN')
+    return token
+
+client.run(get_token())
 
