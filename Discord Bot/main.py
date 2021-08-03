a0516038df8cc5d43bd37a6b1a64ca6533423211
Ethan <ez272@cam.ac.uk>
2021-08-03 15:28:26 +0100

    Changed client to bot using discord.ext. Chaged configure to a command. added 'EM' command prefix. add basic authentication to configure.

--- Discord Bot/main.py
+++ Discord Bot/main.py
@@ -1,44 +1,71 @@
 # IMPORT DISCORD LIBRARIES
 import discord
+from discord.ext import commands
 
 # IMPORT STANDARD LIBRARIES
 import numpy as np
 import os
 
-
-
-# INITIALISE CLIENT
-client = discord.Client()
+#INITALISE BOT COMMANDS
+bot = commands.Bot(command_prefix="EM")
   
 # INITIALISE VARIABLES
 accessible_channels = []
+email_channel = None
+roles_with_access = []
 
-# START UP EVENT
-@client.event
-async def on_ready():
-    print("{0.user} has arrived!".format(client))
-    channels = client.get_all_channels()
-    update_usable_channels(channels)
 
-@client.event
-async def on_message(message):
-    author = message.author
-    channel = message.channel
+# COMMANDS
+@bot.command()
+async def configure(context):
+
+     # GET MESSAGE CONTEXT
+    author = context.author
+    channel = context.channel
     guildID = channel.guild.id 
 
-
-    if not message.content.startswith('!EMConfigure'): return
-    if not author.permissions_in(channel).administrator: return
-    print("hi")
+    # AUTHENTICATE MESSAGE
+    if roles_with_access == []:
+        if not author.permissions_in(channel).administrator: return
+    else: 
+        has_role = False
+        for role in author.roles:
+            if role in roles_with_access: 
+                has_role = True
+                break
+        if not has_role: return
     
+    await channel.send('Authentication complete!')
+    await channel.send('....')
 
+    # ACCESS CONTROL CONFIGURATION
+    await channel.send('What roles should be able to access Emailia?')
+
+
+    # EMAIL CONTROL CONFIGURATION
+
+# START UP EVENT
+@bot.event
+async def on_ready():
+    print("{0.user} has arrived!".format(bot))
+    channels = bot.get_all_channels()
+    update_usable_channels(channels)
+
+# ON MESSAGE EVENT
+@bot.event
+async def on_message(message):
+
+    # GO TO COMMANDS IF POSSIBLE
+    await bot.process_commands(message)
+    
+    
 
 # FIND ALL CHANNELS ACCESABLE BY EMAILIA          
 def update_usable_channels(channels): 
     for channel in channels:
         if channel.category == None: continue
         if channel.category.name != "Text channels": continue
-        if client.user not in channel.members: continue
+        if bot.user not in channel.members: continue
         accessible_channels.append(channel)
 
 # PRINT OUT ALL CHANNELS ACCESSIBLE BY EMAILIA
@@ -52,5 +79,5 @@ def get_token():
     return token
 
 # RUN EMAILIA
-client.run(get_token())
+bot.run(get_token())
 
